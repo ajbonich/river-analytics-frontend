107c309787298000069b2fe877b4361205e30869
Andrew Bonich <ajbonich@gmail.com>
2021-05-10 08:55:41 -0600

    Removed unused files.

--- src/app/api/USGSDataService.jsx
+++ src/app/api/USGSDataService.jsx
@@ -1,7 +1,5 @@
-const baseProdApi = 'https://x7tt9f86r8.execute-api.us-east-2.amazonaws.com/dev'
-const baseLocalApi = 'localhost:8888'
-export function GetUSGSDailyAverageData(siteId) {
-    fetch(`${baseLocalApi}/badgetDailyAverageData?siteId=${siteId}`)
+export function GetUSGSDailyAverageData(baseApi, siteId) {
+    fetch(`${baseApi}/getDailyAverageData?siteId=${siteId}`)
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
